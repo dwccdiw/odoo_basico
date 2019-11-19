2f813c1b7147a307bd89b255c442c4fea1fa73ab
eu <dwccdiw@gmail.com>
2019-11-19 23:23:10 +0100

    importar fotos

--- models/relacion.py
+++ models/relacion.py
@@ -11,11 +11,13 @@ class relacion (models.Model):
 
     moeda_id = fields.Many2one ('res.currency')
     orzamento = fields.Monetary ("Orzamento", 'moeda_id')
-
-
     informacion_ids = fields.Many2many('odoo_basico.informacion', relation='odoo_basico_relacion_informacion',
                                      column1='relacion_id',column2='informacion_id',
                                      ondelete='set null',string="Informacións" )
 
-
+    # moeda_euro_id = fields.Many2one ('res.currency', compute='_eur', domain="[('name','=','EUR')]", store=False)
+    # orzamento_euros = fields.Monetary ("Orzamento en Euros", 'moeda_euro_id')
+    # def _eur(self):
+    #     for rexistro in self:
+    #         rexistro.moeda_euro_id = rexistro.env['res.currency'].browse (rexistro.factura_ids[0].id)
 
