544c17e83c5474fbb83f49dff97891a29a6e927c
dwccdiw <dwccdiw@gmail.com>
2022-01-18 10:43:45 +0100

    report

--- __manifest__.py
+++ __manifest__.py
@@ -29,6 +29,7 @@
         'views/informacion.xml',
         'views/suceso.xml',
         'views/templates.xml',
+        'reports/report_informacion.xml',
         'views/menu.xml',
         'security/ir.model.access.csv',
     ],
